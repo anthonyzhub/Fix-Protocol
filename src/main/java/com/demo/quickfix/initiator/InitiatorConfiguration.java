a55c050e488cc7b674eb983de931e88978171759
Anthony <zamora.anthony1234@gmail.com>
2025-11-24 19:01:28 -0500

    Loaded session settings

--- src/main/java/com/demo/quickfix/initiator/InitiatorConfiguration.java
+++ src/main/java/com/demo/quickfix/initiator/InitiatorConfiguration.java
@@ -2,8 +2,10 @@ package com.demo.quickfix.initiator;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import quickfix.ConfigError;
 import quickfix.DefaultMessageFactory;
 import quickfix.MessageFactory;
+import quickfix.SessionSettings;
 
 @Configuration
 public class InitiatorConfiguration {
@@ -11,5 +13,10 @@ public class InitiatorConfiguration {
     @Bean
     public MessageFactory messageFactory() {
         return new DefaultMessageFactory();
+    }
+
+    @Bean
+    public SessionSettings sessionSettings() throws ConfigError {
+        return new SessionSettings("quickfixj-initiator.cfg");
     }
 }
